41360edb38a4d8215b5abce260807682c7b68d3c
josegarciafern <jose.garciafern@atmira.com>
2021-02-26 11:21:18 +0100

    first commit

---
+++ social-icon.js
@@ -0,0 +1,9 @@
+import { LitElement, html } from 'lit-element';
+
+class SocialIcon extends LitElement {
+    
+    render() {
+        return html`template`
+    }
+}
+customElements.define('social-icon', SocialIcon);
